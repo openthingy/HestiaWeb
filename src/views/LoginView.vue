124c88d3696b8f350a036311f6adce8f3ff4030d
danielalexis <danielalexis.o.pereira@gmail.com>
2023-02-12 14:12:57 +0000

    add initial login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="d-flex justify-center align-center h-screen" >
+ <v-card class="pa-16" width="50vw" max-width="750" title="Login">
+      <v-form
+        v-model="form"
+        @submit.prevent="onSubmit"
+      >
+        <v-text-field
+          v-model="email"
+          :readonly="loading"
+          :rules="[required]"
+          class="mb-2"
+          clearable
+          label="Email"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :readonly="loading"
+          :rules="[required]"
+          clearable
+          label="Password"
+          placeholder="Enter your password"
+        ></v-text-field>
+
+        <br>
+
+        <v-btn
+          :disabled="!form"
+          :loading="loading"
+          block
+          color="success"
+          size="large"
+          type="submit"
+          variant="elevated"
+        >
+          Sign In
+        </v-btn>
+      </v-form>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+ export default {
+    data: () => ({
+      form: false,
+      email: null,
+      password: null,
+      loading: false,
+    }),
+
+    methods: {
+      onSubmit () {
+        if (!this.form) return
+
+        this.loading = true
+
+        setTimeout(() => (this.loading = false), 2000)
+      },
+      required (v: string) {
+        return !!v || 'Field is required'
+      },
+    },
+  }
+</script>
